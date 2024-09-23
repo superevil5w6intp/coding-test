c1ef2a5b57ec241b7dd4cd2cbc05a46174963805
mhong29 <miso_2011@mhong29ui-MacBookPro.local>
2024-09-23 19:09:43 +0900

    if문 완료

--- if문.py
+++ if문.py
@@ -1,1 +1,28 @@
-print ("test")
+def solution():
+    x=input("이름을 입력하세요")
+    if x=="홍미소":
+        print ("안녕 미소야")
+
+    else :
+        print ("뉘쇼")
+
+# solution ()
+
+def solution1():
+    x=int(input("나이를 입력하세요"))
+    if x>12:
+        print('할무니 진지 드세요')
+    else:
+        print ('아기가 울지도 않고 용감하네')
+
+# solution1()
+
+def solution2():
+    x=int(input("나이 입력 ㄱ"))
+    if x>13:
+        print('할무니 진지 드세요')
+    elif x==13:
+        print('우린 칭구칭긔')
+    else:
+        print ('아기가 울지도 않고 용감하네')
+solution2()
